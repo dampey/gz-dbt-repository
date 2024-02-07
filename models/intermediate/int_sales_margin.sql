15add9cafef53b9ea8beb0b8b3fe0d2acd7db9f2
Damien PEYRA <dpeyra@yahoo.fr>
2024-02-07 14:35:35 +0000

    added functions (macros)

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@ SELECT
     quantity, 
     CAST(purchase_price AS FLOAT64) AS purchase_price, 
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin,
+    {{ margin_percent('revenue','purchase_cost') }} AS margin_percent
 FROM {{ref("stg_raw__sales")}} s
 LEFT JOIN {{ref("stg_raw__product")}} p 
     ON s.product_id = p.products_id
